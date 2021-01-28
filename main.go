9878ac79615569f20acc243e48062bdc686638e4
Yiu <wmy@live.hk>
2021-01-28 17:36:05 +0800

    commit

--- main.go
+++ main.go
@@ -12,16 +12,14 @@ import (
 
 const (
 	baseURL        = "http://www.haodoo.net"
-	bookPathPrefix = "?M=book&P="
+	bookPathPrefix = "/?M=book&P="
 	downPathPrefix = baseURL + "/?M=d&P="
 )
 
-func main() {
-	var requestURL, format = os.Args[1], os.Args[2]
+func getBookDownloadPage(url string) <-chan string {
+	l := make(chan string)
 
-	fmt.Println(format)
-
-	res, err := http.Get(requestURL)
+	res, err := http.Get(url)
 
 	if err != nil {
 		log.Fatal(err)
@@ -38,8 +36,51 @@ func main() {
 	go func() {
 		doc.Find("a").Each(func(i int, s *goquery.Selection) {
 			if val, _ := s.Attr("href"); strings.Contains(val, bookPathPrefix) {
+				l <- val
 				fmt.Println(val)
 			}
 		})
+
+		close(l)
 	}()
+
+	return l
+}
+
+func getDownloadUrl(p <-chan string) <-chan string {
+	l := make(chan string)
+
+	go func() {
+		for v := range p {
+			res, err := http.Get(baseURL + "/" + v)
+
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			defer res.Body.Close()
+
+			doc, err := goquery.NewDocumentFromResponse(res)
+
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			fmt.Println(doc)
+		}
+
+		close(l)
+	}()
+
+	return l
+}
+
+func main() {
+	var requestURL, format = os.Args[1], os.Args[2]
+
+	fmt.Println(format)
+
+	p := getBookDownloadPage(requestURL)
+
+	getDownloadUrl(p)
 }
